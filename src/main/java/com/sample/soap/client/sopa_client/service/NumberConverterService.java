43eec13ea336140384887585bdaef6c678a5e439
Aravind Sundarraj <aravind.ssr@gmail.com>
2025-04-27 09:01:06 -0400

    second commit

--- src/main/java/com/sample/soap/client/sopa_client/service/NumberConverterService.java
+++ src/main/java/com/sample/soap/client/sopa_client/service/NumberConverterService.java
@@ -3,15 +3,34 @@ package com.sample.soap.client.sopa_client.service;
 
 import com.example.generated.NumberConversion;
 import com.example.generated.NumberConversionSoapType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+/*
+Can we use NumberConversionSoapType directly without NumberConversion?
+No, you cannot directly instantiate NumberConversionSoapType because:
+
+NumberConversionSoapType is just an interface.
+
+It does not have an implementation.
+
+CXF at runtime will create a proxy (dynamic implementation) using the NumberConversion service class.
+
+📦 Why NumberConversion service = new NumberConversion(); is needed?
+The NumberConversion class is generated to load the WSDL.
+
+It creates a dynamic proxy for the interface NumberConversionSoapType.
+
+It knows which WSDL, which endpoint, and how to bind everything together.
+ */
 @Service
 public class NumberConverterService {
 
-    private final NumberConversionSoapType calculatorSoap = null;
+
 
     public String NumberConverterServices() {
         NumberConversion service = new NumberConversion();
